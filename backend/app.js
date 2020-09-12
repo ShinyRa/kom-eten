8ddf5ecead7df845693974fd7d76a291e57cf1ba
T. de Graaff <tijs.de.graaff@hva.nl>
2020-09-12 14:19:16 +0200

    Added test routing to backend

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var etenRouter = require("./routes/eten");
+var testRouter = require("./routes/test");
 var app = express();
 
 // view engine setup
@@ -20,6 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/eten", etenRouter);
+app.use("/test", testRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
