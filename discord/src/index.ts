24dc4aa9d07da8cddbad6c651ef45257a24ecbcf
T. de Graaff <tijs.de.graaff@hva.nl>
2020-09-12 13:45:41 +0200

    Changed otterbot to mambot

--- discord/src/index.ts
+++ discord/src/index.ts
@@ -10,20 +10,18 @@ const logger: OtterLogger = new OtterLogger(IO);
 
 let welcome: String = String.raw`
 
-
- ______   ______  ______  ______   ______   ______   ______   ______  
-/\  __ \ /\__  _\/\__  _\/\  ___\ /\  == \ /\  == \ /\  __ \ /\__  _\ 
-\ \ \/\ \\/_/\ \/\/_/\ \/\ \  __\ \ \  __< \ \  __< \ \ \/\ \\/_/\ \/ 
- \ \_____\  \ \_\   \ \_\ \ \_____\\ \_\ \_\\ \_____\\ \_____\  \ \_\ 
-  \/_____/   \/_/    \/_/  \/_____/ \/_/ /_/ \/_____/ \/_____/   \/_/ 
-
-
+ __    __     ______     __    __     ______     ______     ______  
+/\ "-./  \   /\  __ \   /\ "-./  \   /\  == \   /\  __ \   /\__  _\ 
+\ \ \-./\ \  \ \  __ \  \ \ \-./\ \  \ \  __<   \ \ \/\ \  \/_/\ \/ 
+ \ \_\ \ \_\  \ \_\ \_\  \ \_\ \ \_\  \ \_____\  \ \_____\    \ \_\ 
+  \/_/  \/_/   \/_/\/_/   \/_/  \/_/   \/_____/  \/_____/     \/_/ 
+                                                                    
 `;
 
 logger.writeToConsole(welcome.toString());
 logger.report(`Version: [${version}]`);
-logger.report(`Lauching otterbot in ${ENV} environment...`);
-logger.report(`Logging in otterbot...`);
+logger.report(`Lauching mambot in ${ENV} environment...`);
+logger.report(`Logging in mambot...`);
 
 if (ENV === "production") {
   console = console || {};
@@ -33,28 +31,8 @@ if (ENV === "production") {
 const bot = new OtterBot(logger);
 
 process.on("SIGINT", () => {
-  logger.report(`Logging out otterbot...`);
+  logger.report(`Logging out mambot...`);
   bot.logout();
   logger.report(`Goodbye!`);
   process.exit(0);
 });
-
-// //To be or not to be otter day. That is the question.
-// schedule.scheduleJob("0 0 * * *", () => {
-//   //for read-ability I have split the assignment of the isOtterday variable and the bound actions into 2 expressions.
-//   isOtterday ? (isOtterday = false) : (isOtterday = true);
-//   if (isOtterday) {
-//     bot.user.setActivity("Het is otterdag!", {
-//       type: "STREAMING",
-//       url: "https://www.youtube.com/watch?v=OjqyQr1Fa4g",
-//     });
-//     bot.channels.cache
-//       .get("738031504940335194")
-//       .send("@everyone Het is otter dag!");
-//   } else {
-//     bot.user.setActivity("Het is geen otter dag...");
-//     bot.channels.cache
-//       .get("738031504940335194")
-//       .send("@everyone Otter dag is afgelopen...");
-//   }
-// });
